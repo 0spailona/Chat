81b055fb4908678a7c951b324bc2b05d9683b4f0
Daria <n-d-p@mail.ru>
2024-03-12 22:21:00 +0300

    change url

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -5,7 +5,7 @@ import MessageView from "./MessageView";
 export default class Chat {
   constructor(container) {
     this.container = container;
-    this.api = new ChatAPI('http://localhost:10000', {
+    this.api = new ChatAPI('https://chatapi-puv7.onrender.com', {
       onOpenConnection: this.onOpenConnection.bind(this),
       onError: this.showAlert.bind(this),
       onRenderChat: this.renderChatWindows.bind(this),
